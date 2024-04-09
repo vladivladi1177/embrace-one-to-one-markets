3b00214eec01488d3c5ef737303726ea77bf5382
vladivladi1177 <161205114+vladivladi1177@users.noreply.github.com>
2024-04-09 03:04:22 +0200

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,29 @@
+        }
+        for (int item : data) {
+    public static void main(String[] args) {
+public class RandomDataGenerator {
+    }
+            System.out.println("Random Number: " + item);
+}
+            data[i] = (int) (Math.random() * 100) + 1;
+
+        }
+        int[] data = new int[10];
+
+        for (int i = 0; i < 10; i++) {    data.forEach(item => {
+    });
+    }
+}
+        console.log("Random Number: " + item);
+    for (let i = 0; i < 10; i++) {
+
+
+
+}
+function generateRandomData() {
+function main() {
+main();
+    return data;
+    const data = generateRandomData();
+    const data = [];
+        data.push(Math.floor(Math.random() * 100) + 1);
